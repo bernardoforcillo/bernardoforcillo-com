34e5948e814e4add9933a8346b7f31c20177056e
Bernardo Forcillo <bernardoforcillo@outlook.com>
2025-05-11 11:04:06 +0200

    chore: update dependencies to use catalog references
     - add new components

--- apps/www/src/components/organisms/navbar/index.tsx
+++ apps/www/src/components/organisms/navbar/index.tsx
@@ -1,3 +1,34 @@
+import Link from 'next/link';
+
+type NavLink = {
+  label: string;
+  href: string;
+};
+
+const mainNavLinks: NavLink[] = [
+  { label: 'Home', href: '/' },
+  { label: 'Blog', href: '/blog' },
+  { label: 'Project', href: '/projects' },
+  { label: 'Notes', href: '/notes' },
+  { label: 'About', href: '/about' },
+];
+
 export default function Navbar() {
-  return <></>;
+  return (
+    <>
+      <header className='sticky flex top-0 z-50'>
+        <div className='mx-auto m-8 rounded-lg w-fit px-3 bg-neutral-300/60 backdrop-blur-xl'>
+          <ol className='flex gap-4'>
+            {mainNavLinks.map((el, indx) => (
+              <>
+                <li key={`link-${indx * 10}`} className='py-3 px-2'>
+                  <Link href={el.href}>{el.label}</Link>
+                </li>
+              </>
+            ))}
+          </ol>
+        </div>
+      </header>
+    </>
+  );
 }
